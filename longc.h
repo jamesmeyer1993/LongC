d7d397ee372b3e2192a2afc75179ba046386fb95
jamesmeyer1993 <jmeyer1993@outlook.com>
2018-09-14 10:17:00 -0500

    updates

---
+++ longc.h
@@ -0,0 +1,10 @@
+#ifndef _LONGC_H_
+#define _LONGC_H_
+
+#define LANG_PRIMITIVES_H     "LongC/lang/primitves.h"
+#define UTIL_ARRAY_H          "LongC/util/array.h"
+#define UTIL_ARRAY_iu8_H      "LongC/util/array/iu8_array.h"
+#define UTIL_ARRAY_iu32_H     "LongC/util/array/iu32_array.h"
+#define UTIL_ARRAY_string_H   "LongC/util/array/string_array.h"
+
+#endif

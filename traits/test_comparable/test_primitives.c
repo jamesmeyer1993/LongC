fd22d2865ceadd772dc50987436ae50ad78f6528
jamesmeyer1993 <jmeyer1993@outlook.com>
2018-12-04 05:41:13 -0600

    Adjusted comparable.h to remove inline function

--- traits/test_comparable/test_primitives.c
+++ traits/test_comparable/test_primitives.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include <longc/lang.h>
-#include <longc/traits/trait.h>
 #include <longc/traits/comparable.h>
+#include <longc/lang/primitive.h>
 
 i32 main(void){
 
@@ -9,7 +9,7 @@ i32 main(void){
     u8 x = 1;
     u8 y = 2;
 
-    assert( IMPLEMENTS( COMPARABLE, u8 ) );
+    assert( implements_comparable(u8) );
     assert( cmpr(u8, &x, &y) != 0 );
     // assert( !eq(u8, &x, &y) );
     // assert( approx(u8, &x, &y, 1) );

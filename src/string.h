5adf1be3b49e2f23ae8dbcbda0ac9f550696faf6
jamesmeyer1993 <jmeyer1993@outlook.com>
2018-12-24 19:59:21 -0600

    removed 'extern' as it is not needed

--- src/string.h
+++ src/string.h
@@ -15,58 +15,58 @@ struct string{
 
 typedef struct string String;
 
-extern String* new_string();
+String* new_string();
 
-extern String* new_string_with_capacity(const size_t capacity);
+String* new_string_with_capacity(const size_t capacity);
 
-extern String* new_string_from(const char *str);
+String* new_string_from(const char *str);
 
-extern void init_string(String* self);
+void init_string(String* self);
 
-extern void init_string_from(String* self, const char* str);
+void init_string_from(String* self, const char* str);
 
-extern void free_string(String *self);
+void free_string(String *self);
 
 //	*	*	*	MATH OPERATORS	*	*	*
 
-extern String* string_add(const String *self, const String *other);
+String* string_add(const String *self, const String *other);
 
-extern String* string_sub(const String *self, const String *other);
+String* string_sub(const String *self, const String *other);
 
-extern String* string_mul(const String *self, const String *other);
+String* string_mul(const String *self, const String *other);
 
-extern String* string_div(const String *self, const String *other);
+String* string_div(const String *self, const String *other);
 
-extern String* string_mod(const String *self, const String *other);
+String* string_mod(const String *self, const String *other);
 
-extern String* string_xor(const String *self, const String *other);
+String* string_xor(const String *self, const String *other);
 
 //	*	*	*	Logic Operators	*	*	*
 
-extern u32 string_eq(const String *self, const String *other);
+u32 string_eq(const String *self, const String *other);
 
-extern i32 string_cmpr(const String *self, const String *other);
-
-//	*	*	*	OPERATORS	*	*	*
-
-extern void string_concat(String* self, String* other);
-
-extern String* string_clone(String* self);
-
-extern String* string_substr(const String* self, const u32 begin, const u32 end);
-
-extern void string_shift(String* self, i32 amount);
+i32 string_cmpr(const String *self, const String *other);
 
 //	*	*	*	OPERATORS	*	*	*
 
-extern i32 string_index_of(const String* self, const String* target);
+void string_concat(String* self, String* other);
 
-extern u32 string_ends_with(const String* self, const String* target);
+String* string_clone(String* self);
 
-extern u32 string_starts_with(const String* self, const String* target);
+String* string_substr(const String* self, const u32 begin, const u32 end);
+
+void string_shift(String* self, i32 amount);
+
+//	*	*	*	OPERATORS	*	*	*
+
+i32 string_index_of(const String* self, const String* target);
+
+u32 string_ends_with(const String* self, const String* target);
+
+u32 string_starts_with(const String* self, const String* target);
 
 //	*	*	*	Display	*	*	*
 
-extern void string_print(const String* self);
+void string_print(const String* self);
 
 #endif /* STRING_H_ */
